c4a2646f24b360b69bdb21c793ac25e8275877e2
sunny-sky <422385795@qq.com>
2018-09-25 18:47:06 +0800

    No_520

---
+++ src/leetcode/string/easy/No_520_DetectCapital.java
@@ -0,0 +1,35 @@
+package leetcode.string.easy;
+
+public class No_520_DetectCapital {
+    public boolean detectCapitalUse(String word) {
+        if (word.length() == 1)
+            return true;
+        if (word.charAt(0) <= 'z' && word.charAt(0) >= 'a') {
+            for (int i = 1; i < word.length(); ++i) {
+                if (word.charAt(i) < 'a' || word.charAt(i) > 'z')
+                    return false;
+            }
+            return true;
+        }
+        else {
+            if (word.charAt(1) <= 'z' && word.charAt(1) >= 'a') {
+                for (int i = 2; i < word.length(); ++i) {
+                    if (word.charAt(i) < 'a' || word.charAt(i) > 'z')
+                        return false;
+                }
+                return true;
+            }
+            else {
+                for (int i = 2; i < word.length(); ++i) {
+                    if (word.charAt(i) < 'A' || word.charAt(i) > 'Z')
+                        return false;
+                }
+                return true;
+            }
+        }
+    }
+
+    public boolean detectCapitalUse2(String word) {
+        return word.substring(1).equals(word.substring(1).toLowerCase())||word.equals(word.toUpperCase());
+    }
+}
