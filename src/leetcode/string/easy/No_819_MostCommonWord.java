ba2abc8d57cd90b4b6ef69b10920bab77101b2a6
sunny-sky <422385795@qq.com>
2018-10-07 23:29:41 +0800

    No_918

---
+++ src/leetcode/string/easy/No_819_MostCommonWord.java
@@ -0,0 +1,16 @@
+package leetcode.string.easy;
+
+import java.util.*;
+
+public class No_819_MostCommonWord {
+    public String mostCommonWord(String paragraph, String[] banned) {
+        Set<String> ban = new HashSet<>(Arrays.asList(banned));
+        Map<String, Integer> count = new HashMap<>();
+        String[] words = paragraph.replaceAll("\\W+", " ").toLowerCase().split("\\s");
+        for (String w : words) {
+            if (!ban.contains(w))
+                count.put(w, count.getOrDefault(w, 0) + 1);
+        }
+        return Collections.max(count.entrySet(), Map.Entry.comparingByValue()).getKey();
+    }
+}
