9cf6bb6b1bf09a25f9ab807df71115d705c271ea
sunny-sky <422385795@qq.com>
2018-10-16 16:40:47 +0800

    Update src/leetcode/easy/No_021_MergeTwoSortedLists.java

---
+++ src/leetcode/easy/No_021_MergeTwoSortedLists.java
@@ -0,0 +1,56 @@
+package leetcode.easy;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) { val = x; }
+ * }
+ */
+public class No_021_MergeTwoSortedLists {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+    }
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public ListNode mergeTwoLists1(ListNode l1, ListNode l2) {
+        if (l1 == null)
+            return l2;
+        if (l2 == null)
+            return l1;
+
+        ListNode list = new ListNode(-1);
+        ListNode res = list;
+        while (l1 != null || l2 != null) {
+            if (l1 != null && (l2 == null || l1.val <= l2.val)) {
+                list.next = l1;
+                l1 = l1.next;
+            } else {
+                list.next = l2;
+                l2 = l2.next;
+            }
+            list = list.next;
+        }
+        return res.next;
+    }
+}
