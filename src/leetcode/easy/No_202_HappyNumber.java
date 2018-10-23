26b5a1a4dc2b67c325d748ae56bd7ad7c32156ca
Liang Hao <422385795@qq.com>
2018-10-23 10:12:52 +0800

    Update src/leetcode/easy/No_202_HappyNumber.java

---
+++ src/leetcode/easy/No_202_HappyNumber.java
@@ -0,0 +1,24 @@
+package leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class No_202_HappyNumber {
+    public boolean isHappy(int n) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = n;
+        while (!map.containsKey(sum)) {
+            map.put(sum, 0);
+            int res = 0;
+            while (sum != 0) {
+                int temp = sum % 10;
+                res += temp * temp;
+                sum /= 10;
+            }
+            if (res == 1)
+                return true;
+            sum = res;
+        }
+        return false;
+    }
+}
