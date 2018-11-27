9ff5d35d9c80b73f125bb7dad7cd9245a9a54c15
sunny-sky <422385795@qq.com>
2018-11-27 12:56:37 +0800

    Create No_496_NextGreaterElementI.java

---
+++ src/leetcode/easy/No_496_NextGreaterElementI.java
@@ -0,0 +1,47 @@
+package leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+public class No_496_NextGreaterElementI {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums2.length; i++) {
+            map.put(nums2[i], -1);
+            for (int j = i + 1; j < nums2.length;j++) {
+                if (nums2[j] >nums2[i]) {
+                    map.put(nums2[i], nums2[j]);
+                    break;
+                }
+            }
+        }
+
+        int[] res = new int[nums1.length];
+        for (int k = 0; k < nums1.length;k++) {
+            res[k] = map.get(nums1[k]);
+        }
+
+        return res;
+    }
+
+    public int[] nextGreaterElement2(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> map = new HashMap<>();
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < nums2.length; i ++) {
+            while (!stack.isEmpty() && nums2[i] > stack.peek()) {
+                map.put(stack.pop(), nums2[i]);
+            }
+            stack.push(nums2[i]);
+        }
+
+        int[] res = new int[nums1.length];
+        for (int j = 0; j <nums1.length; j++) {
+            res[j] = map.getOrDefault(nums1[j], -1);
+        }
+
+        return res;
+    }
+}
