6d67de01ffd3522ac7a76768d986a5f336abe661
sunny-sky <422385795@qq.com>
2019-03-22 10:46:07 +0800

    Create Interval.java

---
+++ src/leetcode/utils/Interval.java
@@ -0,0 +1,21 @@
+package leetcode.utils;
+
+/**
+ * @Author: Liang
+ * @Description: 区间
+ * @Date: Created in 10:27 2019/3/22
+ */
+public class Interval {
+    public int start;
+    public int end;
+
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+}
