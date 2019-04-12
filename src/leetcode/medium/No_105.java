75dd3de976c6f86a81f786ef7eca826bac8335c8
sunny-sky <422385795@qq.com>
2019-04-12 15:30:33 +0800

    Create No_105.java

---
+++ src/leetcode/medium/No_105.java
@@ -0,0 +1,41 @@
+package leetcode.medium;
+
+import leetcode.utils.TreeNode;
+
+/**
+ * @Author: Liang
+ * @Description: 根据一棵树的前序遍历与中序遍历构造二叉树。
+ * 例如，给出
+ *                     0 1  2  3  4
+ * 前序遍历 preorder = [3,9,20,15,7]
+ * 中序遍历 inorder = [9,3,15,20,7]
+ * 返回如下的二叉树：
+ *
+ *     3
+ *    / \
+ *   9  20
+ *     /  \
+ *    15   7
+ * @Date: Created in 17:29 2019/4/11
+ */
+public class No_105 {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        return helper(0, 0, inorder.length - 1, preorder, inorder);
+    }
+
+    private TreeNode helper(int preStart, int inStart, int inEnd, int[] preorder, int[] inorder) {
+        if (preStart > preorder.length - 1 || inStart > inEnd) {
+            return null;
+        }
+        TreeNode root = new TreeNode(preorder[preStart]);
+        int inIndex = 0;  // root 在inorder中的位置
+        for (int i = inStart; i <= inEnd; i++) {
+            if (inorder[i] == root.val) {
+                inIndex = i;
+            }
+        }
+        root.left = helper(preStart + 1, inStart, inIndex - 1, preorder, inorder);
+        root.right = helper(preStart + inIndex - inStart + 1, inIndex + 1, inEnd , preorder, inorder);
+        return root;
+    }
+}
