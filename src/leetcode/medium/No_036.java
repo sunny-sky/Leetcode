0a1c870d1ba22affee6e43ad85cb665a2d031a01
sunny-sky <422385795@qq.com>
2019-05-23 22:16:43 +0800

    Create No_036.java

---
+++ src/leetcode/medium/No_036.java
@@ -0,0 +1,24 @@
+package leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class No_036 {
+    public boolean isValidSudoku(char[][] board) {
+        Set seen = new HashSet<>();
+        for (int i = 0; i < 9; ++i) {
+            for (int j = 0; j < 9; j++) {
+                char number = board[i][j];
+                if (number != '.') {
+                    // 任何一项已存在则退出
+                    if (!seen.add(number + " in row " + i) ||
+                            !seen.add(number + " in colum " + j) ||
+                            !seen.add(number + " in block " + i / 3 + "-" + j / 3)) {
+                        return false;
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
