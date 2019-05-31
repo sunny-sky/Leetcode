14a4af2154566a668a6dc79ca0babc766ff91901
sunny-sky <422385795@qq.com>
2019-05-31 17:17:09 +0800

    Create No_077.java

---
+++ src/leetcode/medium/No_077.java
@@ -0,0 +1,25 @@
+package leetcode.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class No_077 {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> combs = new ArrayList<>();
+        combine(combs, new ArrayList<>(), 1, n, k);
+        return combs;
+    }
+
+    private static void combine(List<List<Integer>> combs, List<Integer> comb, int start, int n, int k) {
+        if (k == 0) {
+            combs.add(new ArrayList<Integer>(comb));
+            return;
+        }
+
+        for (int i = start; i <= n; i++) {
+            comb.add(i);
+            combine(combs, comb, i + 1, n, k - 1);
+            comb.remove(comb.size() - 1);
+        }
+    }
+}
