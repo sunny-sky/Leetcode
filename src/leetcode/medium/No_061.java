e926e29b7be6a81568b8fd6cbc61c78e23e16e06
sunny-sky <422385795@qq.com>
2019-05-31 17:17:05 +0800

    Update No_061.java

--- src/leetcode/medium/No_061.java
+++ src/leetcode/medium/No_061.java
@@ -4,17 +4,21 @@ import leetcode.utils.ListNode;
 
 public class No_061 {
     public ListNode rotateRight(ListNode head, int k) {
+        // 空或只有一个节点
         if (head == null || head.next == null) {
             return head;
         }
+        // 添加头指针
         ListNode dummy = new ListNode(0);
         dummy.next = head;
         ListNode  fast = dummy, slow = dummy;
 
         int i;
+        // fast指针至末尾
         for (i = 0; fast.next != null; i++) {
             fast = fast.next;
         }
+        // slow指针至回转处
         for (int j = i - k % i; j > 0; j--) {
             slow = slow.next;
         }
