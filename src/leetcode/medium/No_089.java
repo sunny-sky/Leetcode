14f9c1e4f64663a5cba8719d456638c5d20e41a2
sunny-sky <422385795@qq.com>
2019-07-03 12:45:38 +0800

    Update No_089.java

--- src/leetcode/medium/No_089.java
+++ src/leetcode/medium/No_089.java
@@ -6,7 +6,10 @@ import java.util.List;
 public class No_089 {
     public List<Integer> grayCode(int n) {
         List<Integer> result = new LinkedList<>();
-        for (int i = 0; i < 1<<n; i++) result.add(i ^ i>>1);
+        // i循环2的n次方次
+        for (int i = 0; i < 1 << n; i++) {
+            result.add(i ^ (i >> 1));
+        }
         return result;
     }
 }
